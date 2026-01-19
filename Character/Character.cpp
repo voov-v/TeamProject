7701d9d2edb12f3c2e3967da0c9591643b933d93
voov-v <yumin5482@gmail.com>
2026-01-19 14:28:36 +0900

    2026-01-19
    Character 클래스 작성

---
+++ Character/Character.cpp
@@ -0,0 +1,30 @@
+﻿#include "Character.h"
+
+ACharacter::ACharacter()
+{
+    this->Name = "Unknown";
+
+    this->Hp = 100;
+
+    this->Atk = 10;
+
+    cout << "ACharacter 생성됨: " << Name << " (HP: " << Hp << ")" << endl;
+}
+
+ACharacter::~ACharacter()
+{
+    cout << "ACharacter 소멸됨" << endl;
+}
+
+void ACharacter::Attack()
+{
+    cout << Name << "가 공격합니다." << endl;
+
+}
+
+void ACharacter::TakeDamage(int DamageAmount)
+{
+    Hp -= DamageAmount;
+
+    cout << Name << "가 " << DamageAmount << "의 피해를 입었습니다." << endl;
+}
---
+++ Character/Character.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+
+class ACharacter
+{
+public:
+    ACharacter();
+    ~ACharacter();
+
+protected:
+    string Name;
+    int Hp;
+    int Atk;
+
+public:
+    void Attack();
+
+    void TakeDamage(int DamageAmount);
+
+};
